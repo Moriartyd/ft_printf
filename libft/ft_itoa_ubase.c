702fc3da13d7b897fdebbb0a2fdaf52f6a3b3f6c
Dmitry Moriarty <dima@galeev.ru>
2019-07-05 02:16:22 +0300

    Create ft_itoa_ubase.c

---
+++ libft/ft_itoa_ubase.c
@@ -0,0 +1,31 @@
+#include "libft.h"
+
+static int	ft_num_len_base(int num, int base)
+{
+	int i;
+
+	i = 1;
+	while ((num = num / base) != 0)
+		i++;
+	return (i);
+}
+
+char		*ft_itoa_ubase(size_t num, int base)
+{
+	long	n;
+	int		i;
+	char	*str;
+
+	i = ft_num_len_base(num, base);
+	n = num;
+	if (!(str = malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	str[i] = '\0';
+	while (n)
+	{
+		str[i - 1] = (n % base >= 10) ? n % base + 55 : n % base + '0';
+		n /= base;
+		i--;
+	}
+	return (str);
+}
